ae7e8bb2782c257279a9e7b0882d7a333059e06d
Johanna Erikson <johanna5@tlu.ee>
2023-09-20 15:44:20 +0300

    kolmas tund poolik

---
+++ src/components/Hello.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Hello = props => {
+  return <div>Hello {props.name}</div>
+
+}
+
+export default Hello
+
+// const Hello = props => {
+//     const {name} = props
+
+//     return <div>Hello {name}</div>
+  
+//   }
+  
+
+
+//   const Hello = ({name}) => {
+    
+//     return <div>Hello {name}</div>
+  
+//   }
+  
+
+//   const Hello = ({name}) => <div>Hello {name}!</div>
+//   export default Hello
+
+const Hello = 
